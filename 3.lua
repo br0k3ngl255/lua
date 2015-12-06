ac3c34c27c6f464aa5cf32eec41888d61bdc6891
silent-mobius <michael@linuxsystems.rocks>
2015-12-06 14:31:31 +0200

    learning

---
+++ 3.lua
@@ -0,0 +1,5 @@
+#!/usr/bin/env lua
+
+name='br0k3ngl255'
+
+io.write(" my name is ", name, "\n")
---
+++ 4.lua
@@ -0,0 +1,9 @@
+#!/usr/bin/env lua
+
+bigNum=99933399922255511
+--this is comment 
+-- you need to have comma in order to use variable 
+io.write(" the number is ",bigNum,"\n"  )
+--every type is trated the same in lua
+-- to prove it - we can check it with type() function : --> 
+io.write("the type is ", type(bigNum) ,"\n ")
